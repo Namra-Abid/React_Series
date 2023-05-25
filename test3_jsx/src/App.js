7225812a82e9024c8ac3aa490cae50a9b9983748
Namra  Abid <s.namraabid@gmail.com>
2023-05-25 10:08:06 +0500

    Hello World JSX

--- test3_jsx/src/App.js
+++ test3_jsx/src/App.js
@@ -4,7 +4,7 @@ import  JSXElement from './components/test'
 import Double_JSX_Element from './components/test2'
 import JSX_three_params from './components/test3'
 import Hello_World_JSX from './components/test4'
-
+import HelloNamraJSX from './components/test5'
 function App() {
   return (
     <div className="App">
@@ -12,6 +12,7 @@ function App() {
       <Double_JSX_Element/>
       <JSX_three_params />
       <Hello_World_JSX/>
+      <HelloNamraJSX />
     </div>
   );
 }
---
+++ test3_jsx/src/components/test5.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HelloNamraJSX =() =>{
+    return React.createElement(
+    'div',
+    null,
+    React.createElement(
+    'p',
+    null,
+    React.createElement('b',null,'Hello Namra in JSX Style'))
+    )
+}
+
+export  default HelloNamraJSX
