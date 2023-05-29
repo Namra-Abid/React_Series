7b8eac36beb6d40823232d786586a0de1ca5072d
Namra  Abid <s.namraabid@gmail.com>
2023-05-29 10:28:02 +0500

    little about state

---
+++ test5_state/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ChangeColor  from './components/state_basics1';
+function App() {
+  return (
+    <div className="App">
+      <ChangeColor/>
+    </div>
+  );
+}
+
+export default App;
---
+++ test5_state/src/components/state_basics1.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class ChangeColor extends Component {
+    constructor(){
+        super()
+        this.state={
+            message: 'Example Text Area'
+        }
+    }
+        ChangeMessage(){
+            
+            this.setState({
+                message:'Please write your feedback here !'
+            })
+        }
+
+    
+    render() {
+        return (
+            <div className='mb-3'>
+                <h1>{this.state.message}</h1>
+                <textarea className="form-control" id="myBox" rows="8"></textarea>
+                <br></br>
+                <button onClick={()=> this.ChangeMessage()}> Change Heading</button>
+            </div>
+        )
+    }
+}
+export default ChangeColor
