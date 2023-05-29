c91ad13422dbb24b44b505be942c1a85ff00442b
Namra  Abid <s.namraabid@gmail.com>
2023-05-29 10:33:52 +0500

    Change Text on click

--- test5_state/src/App.js
+++ test5_state/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import ChangeColor  from './components/state_basics1';
+import ChangeText2 from './components/state_basics2';
 function App() {
   return (
     <div className="App">
       <ChangeColor/>
+      <ChangeText2/>
     </div>
   );
 }
---
+++ test5_state/src/components/state_basics2.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+class ChangeText2 extends Component {
+    constructor(){
+        super()
+        this.state={
+            message: 'Example Text Area'
+        }
+    }
+        ChangeMessage(){
+            if (this.state.message==='Example Text Area')
+            { console.log("if")
+            this.setState({
+                message:'Please write your feedback here !'
+                
+            })}
+            else{ console.log("else")
+                this.setState({
+                    message:'Example Text Area'
+                })
+            }
+        }
+
+    
+    render() {
+        return (
+            <div className='mb-3'>
+                <h1>{this.state.message}</h1>
+                <textarea className="form-control" id="myBox" rows="8"></textarea>
+                <br></br>
+                <button onClick={()=> this.ChangeMessage()}> Change Heading</button>
+            </div>
+        )
+    }
+}
+export default ChangeText2
