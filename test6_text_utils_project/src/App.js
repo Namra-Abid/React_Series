d7985395aa9ae2933ffa22ad1cade1fdd744c7d2
Namra  Abid <s.namraabid@gmail.com>
2023-05-31 16:41:07 +0500

    Back to actual coding from default props

--- test6_text_utils_project/src/App.js
+++ test6_text_utils_project/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 function App() {
   return (
-   <Navbar/>
+    <Navbar title="Text Utils Namra" aboutText="About Text Utils"/>
+  
     );
 }
 
