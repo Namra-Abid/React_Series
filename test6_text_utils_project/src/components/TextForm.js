9437277f218008b6793fb130f5159f6644b8b1cd
Namra  Abid <s.namraabid@gmail.com>
2023-06-02 10:47:21 +0500

    made two handlers

--- test6_text_utils_project/src/components/TextForm.js
+++ test6_text_utils_project/src/components/TextForm.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TextForm(props) {
+    const [text,setText]=useState('Enter Text Here');
+    const HandleUpClick =()=>{
+        console.log('Upper Case got clicked ');
+
+    }
+    const HandeOnChange=()=>{
+        console.log('on change')
+    }
+    //text="new text"  // wrong way to change state
+   // setText("New text"); // correct way to change  state
+   
     return (
         <div>
             <h1>{props.heading}</h1>
             <div class="mb-3">
-                <textarea class="form-control" id="myBox" rows="8"></textarea>
+                <textarea class="form-control" value={text} onChange={HandeOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary">Convert to Upper Case</button>
+            <button className="btn btn-primary" onClick={HandleUpClick}>Convert to Upper Case</button>
 
         </div>
     )
