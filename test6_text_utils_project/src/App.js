b54e9cbc2833269d3f04b47a00b16a32531ba5b2
Namra  Abid <s.namraabid@gmail.com>
2023-06-02 10:35:13 +0500

    created basic TextForm.js file as a component

--- test6_text_utils_project/src/App.js
+++ test6_text_utils_project/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 function App() {
   return (
-    <Navbar title="Text Utils Namra" aboutText="About Text Utils"/>
-  
-    );
+    <div>
+      <Navbar title="Text Utils Namra" aboutText="About Text Utils" />
+      <div className='container my-3'>
+      <TextForm heading='Enter Text To Analyze' />
+      </div>
+    </div>
+
+  );
 }
 
 export default App;
