416bb185fc102167db6bf02f2c8b501a1e7e3fd9
Namra  Abid <s.namraabid@gmail.com>
2023-06-08 11:23:33 +0500

    Basics of router and routes

--- test6_text_utils_project/src/components/Navbar.js
+++ test6_text_utils_project/src/components/Navbar.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
@@ -13,10 +14,10 @@ export default function Navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+            <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">{props.aboutText}</a>
+            <Link className="nav-link" to="/about">{props.aboutText}</Link>
           </li>
         </ul>
         {/* <form className="form-inline my-2 my-lg-0">
